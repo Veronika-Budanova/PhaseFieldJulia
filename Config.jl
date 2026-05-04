644ef7da10f3cac06118686167ed0ecf4cc7688f
Veronika-Budanova <veronik.budanova@gmail.com>
2026-05-04 16:53:12 +0300

    Только центральные разности, графики в итоговом формате

--- Config.jl
+++ Config.jl
@@ -1,13 +1,13 @@
 # GRID
-L = 1 #1e-3 
-N = 51 #51 
+L = 2 #1e-3 
+N = 101 #51 
 fict = 3
 h = L / (N - 1)
 
 # PHYSIC PARAMETERS
 #b = 100
 #R = 0.12
-b = 25
+b = 50
 
 rho1A = 90
 rho1B = 1200
@@ -22,8 +22,6 @@ lam22 = lam11
 Apsi = 1e-4
 Bpsi = Apsi
 
-lamelA = 0
-muelA = lamelA
 lamelB = 2e2
 muelB = lamelB
 
@@ -31,19 +29,19 @@ theta = 1
 
 M0 = 1
 
-eta = 10
+eta = 5
 zeta = 0
 
 g = 0.05
 
-u_init = 0.0
+u_init = -0.05
 
 bound = "wall"
 
 # TIME PARAMETERS
 #CFL = 0.125
 
-t_max = 100.0
+t_max = 20
 delta_t = 1e-6
 step_max = t_max/delta_t
 
@@ -52,9 +50,9 @@ frame_dt = 0.1
 # PISTON PARAMETERS
 #gamma = 1.4
 
-piston_width = 0.2 * L
+piston_width = 0.3 * L
 
-x0_piston = 0.5 * L
+x0_piston = 0.6 * L
 #v0_piston = 0.0
 
 #S = 1.0
@@ -62,4 +60,4 @@ x0_piston = 0.5 * L
 #p0 = 100.0
 
 # DIRECTORY
-direct = "CSframes/14"
+direct = "CSframes/28"
